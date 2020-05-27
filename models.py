88fa26e6bb44522cebe9f15bea72b3b5a17c6071
RandyDeo <randydeo@gmail.com>
2020-05-27 13:55:15 +0000

    Added User Model

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
+db = SQLAlchemy()
 
 class Logs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +18,23 @@ class Logs(db.Model):
             'stream': self.stream,
             'created': self.created.strftime("%m/%d/%Y, %H:%M:%S")
         }
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.integer, primary_key=True)
+    username = db.Column(db.string, nullable=False)
+    email = db.Column(db.string, nullable=False)
+    password = db.Column(db.string, nullable=False)
+
+    def toDict(self):
+        return{
+            "ID ": self.id,
+            "Username ": self.username,
+            "Email ": self.email,
+            "Password ": self.password
+        }
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password, method='sha256')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
